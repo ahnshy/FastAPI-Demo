f097693d7a62de43e68628aeba6d6b85c306f1fc
ahnshy <ahnshy@naver.com>
2024-09-28 14:00:18 +0900

    <feat> : Implementation schema.py

--- app/database/schema.py
+++ app/database/schema.py
@@ -21,4 +21,37 @@ class BaseMixin:
     def __hash__(self):
         return hash(self.id)
 
-    def create(self, session: Session, auto_commit=False, **kwargs):
+    #@classmethod
+    def create(cls, session: Session, auto_commit=False, **kwargs):
+        """
+        테이블 데이터 적재 전용 함수
+        :param session:
+        :param auto_commit: 자동 커밋 여부
+        :param kwargs: 적재 할 데이터
+        :return:
+        """
+        obj = cls()
+        for col in obj.all_columns():
+            col_name = col.name
+            if col_name in kwargs:
+                setattr(obj, col_name, kwargs.get(col_name))
+        session.add(obj)
+        session.flush()
+        if auto_commit:
+            session.commit()
+        return obj
+
+class Users(Base, BaseMixin):
+    __tablename__ = "users"
+    status = Column(Enum("active", "deleted", "blocked"), default="active")
+    email = Column(String(length=255), nullable=True)
+    pw = Column(String(length=2000), nullable=True)
+    name = Column(String(length=255), nullable=True)
+    phone_number = Column(String(length=20), nullable=True, unique=True)
+    profile_img = Column(String(length=1000), nullable=True)
+    sns_type = Column(Enum("FB", "G", "K"), nullable=True)
+    marketing_agree = Column(Boolean, nullable=True, default=True)
+    keys = relationship("ApiKeys", back_populates="users")
+
+
+
