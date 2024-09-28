27f06a1adbcbc19961c1ae1501ce90f982747628
ahnshy <ahnshy@naver.com>
2024-09-28 11:25:17 +0900

    <feat> : parameters main.py in app argument

--- app/mian.py
+++ app/mian.py
@@ -1,8 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.src.config import conf
 
+def create_app():
+    app = FastAPI()
+    return app
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    # to do initialize database
+    # to do redis
+    # to do define middle-ware, router ...etc
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=conf().PROJ_RELOAD)
