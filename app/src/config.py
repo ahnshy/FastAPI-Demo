69aa300220d06b2ffa9d6c3f705cfd6f8ca5258b
ahnshy <ahnshy@naver.com>
2024-09-28 11:14:30 +0900

    <feat> : Implementation config.py

---
+++ app/src/config.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, asdict
+from os import path, environ
+
+base_dir = path.dirname(path.dirname(path.abspath(__file__)))
+
+@dataclass
+class Config:
+    BASE_DIR = base_dir
+
+    DB_POOL_RECYCLE: int = 900
+    BE_ECHO: bool = True
+
+@dataclass
+class LocalConfig(Config):
+    PROJ_RELOAD: bool = True
+
+@dataclass
+class ProdConfig(Config):
+    PROJ_RELOAD: bool = False
+
+
+
+def conf():
+    config = dict(prod=ProdConfig(), local=LocalConfig())
+    return config.get(environ.get("API_ENV", "local"))
