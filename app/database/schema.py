582e8dda205094373cff9f6c54c109c34a4a2210
ahnshy <ahnshy@naver.com>
2024-09-28 14:06:18 +0900

    <fix> : fix syntax error

--- app/database/schema.py
+++ app/database/schema.py
@@ -10,10 +10,14 @@ from sqlalchemy import (
     Enum
 )
 
+from sqlalchemy.orm import Session, relationship
+
+from app.database.connect import Base, db
+
 class BaseMixin:
     id = Column(Integer, primary_key=True, index=True)
-    create_at = Column(Datetime, nullable=False, default=func.utc_timestamp())
-    modified_at = Column(Datetime, nullable=False, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
+    create_at = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    modified_at = Column(DateTime, nullable=False, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
 
     def all_column(self):
         return [c for c in self.__table__.columns if c.primary_key is False and c.name != "create_at"]
