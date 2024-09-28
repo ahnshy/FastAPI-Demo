2459f58048a26213f83ce8b1e87a274a1d1e0f2a
ahnshy <ahnshy@naver.com>
2024-09-28 13:13:20 +0900

    <feat> : add db instance main.py

--- app/mian.py
+++ app/mian.py
@@ -1,15 +1,24 @@
+from dataclasses import asdict
+
 import uvicorn
 from fastapi import FastAPI
 
+from app.database.connect import db
 from app.src.config import conf
+from app.router import index
 
 def create_app():
+    c = conf()
     app = FastAPI()
-    return app
+    conf_dict = asdict(c)
+    db.init_app(app, **conf_dict)
 
     # to do initialize database
     # to do redis
     # to do define middle-ware, router ...etc
+
+    app.include_router(index.router)
+    return app
 
 app = create_app()
 
